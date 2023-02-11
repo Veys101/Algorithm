834ab76c0050c6094fac640ab9aa5b16e53b54f4
Veys101 <vys2626@gmail.com>
2023-02-12 01:27:48 +0300

    reformating

--- heaps_algorithm.cpp
+++ heaps_algorithm.cpp
@@ -1,90 +1,104 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
 
-void fillEmpty (char* input, int len) {
+void fillEmpty(char *input, int len)
+{
 	memset(input, '\0', len);
-	input[len]='\0';
+	input[len] = '\0';
 }
 
-void initializeArray (char** &input, int lengthArray, int lengthRow) {
-	
-	input = new char*[lengthArray];
-	
-	for (int i=0; i<lengthArray; i++) {
-		input[i] = new char [lengthRow]; 
+void initializeArray(char **&input, int lengthArray, int lengthRow)
+{
+
+	input = new char *[lengthArray];
+
+	for (int i = 0; i < lengthArray; i++)
+	{
+		input[i] = new char[lengthRow];
 		fillEmpty(input[i], lengthRow);
 	}
-	
 }
 
-int fact (int number) {
-	
+int fact(int number)
+{
+
 	int result = 1;
-	
-	while (number > 1) {
+
+	while (number > 1)
+	{
 		result *= number;
 		number--;
 	}
-	
+
 	return result;
 }
 
-void swapElements (char* &elements, int i, int j) {
-	
+void swapElements(char *&elements, int i, int j)
+{
+
 	char temp = elements[i];
 	elements[i] = elements[j];
 	elements[j] = temp;
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
+	char *input = argv[1];
 
-	char* input = argv[1];
-	
 	int length = strlen(input);
 	int lenP = fact(length);
-	
-	char** generatedPermutation;
-    initializeArray (generatedPermutation, lenP, length);
+
+	char **generatedPermutation;
+	initializeArray(generatedPermutation, lenP, length);
 
 	int c[length] = {0};
-	
-	int i = 0, a=0;
+
+	int i = 0, a = 0;
 	int index = 1;
-	
-	for(int k=0; k<length; k++) generatedPermutation[0][k] = input[k];
-		
-	while (i < length) {
-		
-		if (c[i] < i) {
-						
-			if (i % 2 == 0) {
+
+	for (int k = 0; k < length; k++)
+		generatedPermutation[0][k] = input[k];
+
+	while (i < length)
+	{
+
+		if (c[i] < i)
+		{
+
+			if (i % 2 == 0)
+			{
 				swapElements(input, 0, i);
-			} else {
+			}
+			else
+			{
 				swapElements(input, c[i], i);
 			}
 
-			for (int j=0; j<length; j++) {
+			for (int j = 0; j < length; j++)
+			{
 				generatedPermutation[index][j] = input[j];
 				a++;
 			}
-			
+
 			c[i] = c[i] + 1;
 			i = 0;
 			index++;
-		} else {
+		}
+		else
+		{
 			c[i] = 0;
 			i++;
 			a++;
-		}	
-				
-	} 
-	
-	for (int k = 0; k < lenP; k++) {
+		}
+	}
+
+	for (int k = 0; k < lenP; k++)
+	{
 		cout << generatedPermutation[k] << endl;
 	}
-	
+
 	cout << "Number of loop for " << length << " permutation : " << a << endl;
 }
--- johnson-trotter-algorithm.cpp
+++ johnson-trotter-algorithm.cpp
@@ -1,131 +1,154 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int a = 0;
 
-int fact(int n) {	
-	return (n == 1) || (n == 0) ? 1 : n * fact(n-1);
+int fact(int n)
+{
+	return (n == 1) || (n == 0) ? 1 : n * fact(n - 1);
 }
 
-void initializeArray (char** generatedPermutation, int lenP, int length) {
-	
-	for (int i = 0; i < lenP; i++) {
+void initializeArray(char **generatedPermutation, int lenP, int length)
+{
+
+	for (int i = 0; i < lenP; i++)
+	{
 		generatedPermutation[i] = new char[length];
 		memset(generatedPermutation[i], '0', length);
 		generatedPermutation[i][length] = '\0';
 	}
-	
 }
 
-void fillOne (int* input, int length) {
-	for (int i=0; i<length; i++) {
+void fillOne(int *input, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
 		input[i] = 1;
 	}
 }
 
-int getLargestMobileComponentIndex (char* input, int* direction, int length) {
-	
+int getLargestMobileComponentIndex(char *input, int *direction, int length)
+{
+
 	int largestMobileComponentIndex = -1;
 	char largestMobileComponent = '\0';
-	
-	for(int i = 0; i < length; i++) {
-		
-		if (direction[i] == 0) {
-			
-			if (i == (length - 1)) continue;
-			
-			if ((input[i] > input[i+1]) && (input[i] > largestMobileComponent)) {
+
+	for (int i = 0; i < length; i++)
+	{
+
+		if (direction[i] == 0)
+		{
+
+			if (i == (length - 1))
+				continue;
+
+			if ((input[i] > input[i + 1]) && (input[i] > largestMobileComponent))
+			{
 				largestMobileComponentIndex = i;
 				largestMobileComponent = input[i];
 			}
-			
-		} else {
-			
-			if (i == 0) continue;
+		}
+		else
+		{
 
-			if ((input[i] > input[i - 1]) && (input[i] > largestMobileComponent)) {
+			if (i == 0)
+				continue;
+
+			if ((input[i] > input[i - 1]) && (input[i] > largestMobileComponent))
+			{
 				largestMobileComponentIndex = i;
 				largestMobileComponent = input[i];
 			}
 		}
 		a++;
 	}
-	
-	return largestMobileComponentIndex; 
+
+	return largestMobileComponentIndex;
 }
 
-void swapElements (char* input, int* direction, int firstIndex, int secondIndex) {	
+void swapElements(char *input, int *direction, int firstIndex, int secondIndex)
+{
 	char temp1 = input[secondIndex];
 	input[secondIndex] = input[firstIndex];
 	input[firstIndex] = temp1;
-	
+
 	int temp2 = direction[secondIndex];
 	direction[secondIndex] = direction[firstIndex];
 	direction[firstIndex] = temp2;
 }
 
-void reverseDirection (int largestComponentIndex, char* input, int* direction, int length) {
-	for (int i = 0; i < length; i++) {
-		if(input[largestComponentIndex] < input[i]) {
+void reverseDirection(int largestComponentIndex, char *input, int *direction, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		if (input[largestComponentIndex] < input[i])
+		{
 			direction[i] = !direction[i];
 		}
 		a++;
 	}
 }
 
-int main(int argc, char** argv) {
-	
-	char* input = argv[1];
+int main(int argc, char **argv)
+{
+
+	char *input = argv[1];
 	int length = strlen(input);
 	int lenP = fact(length);
-	char** generatedPermutation = new char*[lenP];
-	
+	char **generatedPermutation = new char *[lenP];
+
 	initializeArray(generatedPermutation, lenP, length);
-	
+
 	// direction 0 : right arrow
 	// direction 1 : left arrow
-	int* direction = new int [length];
+	int *direction = new int[length];
 	fillOne(direction, length);
-	
-	for (int i = 0; i < length; i++) generatedPermutation[0][i] = input[i]; 
-	
+
+	for (int i = 0; i < length; i++)
+		generatedPermutation[0][i] = input[i];
+
 	int index = 1;
 	int largestComponentIndex = getLargestMobileComponentIndex(input, direction, length);
 	char mobileComponent = input[largestComponentIndex];
-	
-	while (largestComponentIndex != -1) {
 
-		if (direction[largestComponentIndex] == 0) {
-			if (largestComponentIndex != (length - 1)) {
-				swapElements(input, direction, largestComponentIndex, largestComponentIndex+1);
+	while (largestComponentIndex != -1)
+	{
+
+		if (direction[largestComponentIndex] == 0)
+		{
+			if (largestComponentIndex != (length - 1))
+			{
+				swapElements(input, direction, largestComponentIndex, largestComponentIndex + 1);
 				largestComponentIndex += 1;
 			}
 		}
-		else {
-			if (largestComponentIndex != 0) {                                    
-				swapElements(input, direction, largestComponentIndex, largestComponentIndex-1);
+		else
+		{
+			if (largestComponentIndex != 0)
+			{
+				swapElements(input, direction, largestComponentIndex, largestComponentIndex - 1);
 				largestComponentIndex -= 1;
-			}	
+			}
 		}
-		
-		for (int j = 0; j < length; j++) {
+
+		for (int j = 0; j < length; j++)
+		{
 			generatedPermutation[index][j] = input[j];
 		}
-				
+
 		reverseDirection(largestComponentIndex, input, direction, length);
 		largestComponentIndex = getLargestMobileComponentIndex(input, direction, length);
 		mobileComponent = input[largestComponentIndex];
 		index++;
 		a++;
-		
 	}
-	
-	for (int i = 0; i < lenP; i++) {
+
+	for (int i = 0; i < lenP; i++)
+	{
 		cout << generatedPermutation[i] << endl;
 	}
-	
+
 	cout << "Number of loop for " << length << " permutation : " << a << endl;
- 
 }
